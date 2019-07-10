5ba5ed260e3a9b7852c06887f20fa567a9954647
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-07-11 03:35:53 +0900

    Minimal Boost.NumPy example

---
+++ flat.cpp
@@ -0,0 +1,18 @@
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+
+namespace p = boost::python;
+namespace np = boost::python::numpy;
+
+np::ndarray new_zero1(unsigned int N) {
+  p::tuple shape = p::make_tuple(N);
+  np::dtype dtype = np::dtype::get_builtin<double>();
+  return np::zeros(shape, dtype);
+}
+
+BOOST_PYTHON_MODULE(flat_example) {
+  Py_Initialize();
+  np::initialize();
+
+  p::def("new_zero", new_zero1);
+}
